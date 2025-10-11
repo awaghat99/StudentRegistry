6b456e4433a2ce615b26c52baa17c4f91a6b0a9a
Aadil Waghat <a.waghat16@gmail.com>
2025-10-11 20:27:47 +0100

    feat: implement Student model(fields, ctor, gettor, toString)

---
+++ src/main/com/aadil/studentregistry/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private String id;
+    private String name;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    @Override
+    public String toString() {
+    return "ID: " + id + ", Name: " + name;
+}
+
+}
